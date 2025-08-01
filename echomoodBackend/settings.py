7b6cd3ce890ec83da2972a73114396ddc643c8a8
Ian Rugi <ianojra2@gmail.com>
2025-08-01 22:06:09 +0300

    connect to render

--- echomoodBackend/settings.py
+++ echomoodBackend/settings.py
@@ -89,6 +89,13 @@ WSGI_APPLICATION = 'echomoodBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
